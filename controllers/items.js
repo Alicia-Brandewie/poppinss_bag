a9e48a4e6bf360be0aae2ea47672e1e011353e43
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-07-29 19:55:57 -0500

    show-item data and UI working

--- controllers/items.js
+++ controllers/items.js
@@ -7,12 +7,12 @@ const User = require('../models/user.js');
 
 
 //POST_ new item
-router.post ('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-    const currentUser = await User.findById(req.session.user._id);
-    currentUser.catalog.push(req.body);
-    await currentUser.save();
-    res.redirect(`/users/${currentUser._id}/items`);
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.catalog.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/items`);
     } catch (error) {
         console.log(error);
         res.redirect('/');
@@ -37,16 +37,59 @@ router.get('/', async (req, res) => {
 });
 
 //GET_ new item
-router.get('/new', async (req,res) =>{
+router.get('/new', async (req, res) => {
     res.render('items/new.ejs')
 });
 
 
 
+//GET_to show-item.ejs
+router.get('/:itemId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const showItem = currentUser.catalog.id(req.params.itemId);
+        res.render('items/show-item.ejs', {
+            addedItem: showItem,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+
+//GET_item to edit
+router.get('/:catalogId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id); //find current user
+        const catalog = currentUser.catalog.id(req.params.catalogId);//find clicked item//
+        res.render('/items/edit.ejs', {
+            addedItem: catalog,
+        });       //send change//
+        res.redirect(`/users/${currentUser._id}/items/${req.catalogId}`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 
 
 /*-------------------- UPDATE _ Router logic ---------------------*/
+//PUT_edited item into DB
+// router.get('/', async (req, res) => {
+//     try {
+//         const currentUser = await User.findById(req.session.user._id); //find current user
+//         const catalog = currentUser.catalog.id(req.params.catalogId);//find clicked item//
+//         catalog.set(req.body);
+//         await currentUser.save();        //send change//
+//         res.redirect(`/users/${currentUser._id}/items/${req.catalogId}`);
+//     } catch (error) {
+//         console.log(error);
+//         res.redirect('/');
+//     }
+// });
+
 
 /*-------------------- DELETE _ Router logic ---------------------*/
 
