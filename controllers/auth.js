a3c0640b54077db526c85b9838139ca785acbbba
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-08-05 09:43:58 -0500

    auth sign up redirect

--- controllers/auth.js
+++ controllers/auth.js
@@ -23,7 +23,7 @@ router.post('/sign-up', async (req, res) => {
 
     const user = await User.create(req.body);
 
-    res.send(`Thanks for signing up ${user.username}`);
+    res.redirect("/auth/sign-in");
 })
 
 router.get("/sign-in", (req, res) => {
